9c89f7d2d2e836ac4f2b2fa22d41994855a358d0
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2020-12-17 22:42:29 -0500

    added get and API js file to reach the server

--- youtubesapp/src/App.js
+++ youtubesapp/src/App.js
@@ -1,5 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
+
+import API from '../api';
+
+export default class tubesList extends React.Component {
+  handleSubmit = event => {
+    event.preventDefault();
+
+    API.delete('users/${this.state.id}')
+    .then(res => {
+      .then(res => {
+        console.log(res);
+        console.log(res.data)
+    
+    })
+
+    const user = {
+      name: this.state.name
+    };
+
+    axios.post('https://jsonplaceholder.typicode.com/users', { user })
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+  }
+  render(){
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Search :
+            <input type="text" onChange={this.handleChange} />
+          </label>
+          <button type="submut">Add</button>
+        </form>
+      </div>
+    )
+  }
+  componentDidMount() {
+    axios.get('http://jsonplaceholder.typicode.com/users')
+    .then(res => {
+      const lists = res.data;
+      this.setState({ lists });
+    })
+  }
+  render() {
+    return (
+      <ul>
+        { this.state.lists.map(list => <li>{list.name}</li>)}
+      </ul>
+    )
+  }
+}
 
 function App() {
   return (
