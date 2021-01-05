40a62e022832a9880185f798ff22250c1dd5b7d6
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2021-01-05 08:30:22 -0500

    added titlebar jsx

---
+++ backend/youtube-clone/src/components/titleBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+functionTitleBar(props) {
+    return (
+        <div className='rowrow-spacer'>
+            <div className='col-md-12'style={{padding:0}}>
+                <div>
+                    <h1>dCCLibrary</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default TitleBar;
